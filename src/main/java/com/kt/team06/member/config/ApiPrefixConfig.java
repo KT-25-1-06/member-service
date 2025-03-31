b3ef0529f395ba73fe1daf29521c3424b1a995af
kimjunseok <junsuk8960@naver.com>
2025-03-31 15:45:26 +0900

    feat: prefix url 설정

---
+++ src/main/java/com/kt/team06/member/config/ApiPrefixConfig.java
@@ -0,0 +1,14 @@
+package com.kt.team06.member.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiPrefixConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/members", c -> true);
+    }
+}
